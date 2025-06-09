f9b1bbcd2f2cd700267d2fdbe943b6cebb891cd3
YutoYasujima <apyfm.3.25.y.y@gmail.com>
2025-06-09 16:21:20 +0900

    fixed: 登録メール再送時バリデーションエラーをフラッシュメッセージで表示

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -7,9 +7,20 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   # end
 
   # POST /resource/confirmation
-  # def create
-  #   super
-  # end
+  def create
+    self.resource = resource_class.send_confirmation_instructions(resource_params)
+    yield resource if block_given?
+
+    if successfully_sent?(resource)
+      # 通常通りリダイレクト（flash[:notice] は Devise が自動で設定）
+      respond_with({}, location: after_resending_confirmation_instructions_path_for(resource_name))
+    else
+      # バリデーションエラー時に flash を手動設定
+      # オーバーライドで下記の１行だけ追記
+      flash.now[:alert] = resource.errors.full_messages
+      respond_with(resource)
+    end
+  end
 
   # GET /resource/confirmation?confirmation_token=abcdef
   # def show
