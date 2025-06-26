845d5e881d4b945804db97e79a103d409a2a0e4c
YutoYasujima <apyfm.3.25.y.y@gmail.com>
2025-06-26 21:19:20 +0900

    fixed:Lintチェック修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable, :omniauthable, omniauth_providers: [:google_oauth2]
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable, :omniauthable, omniauth_providers: [ :google_oauth2 ]
 
   has_one :profile, dependent: :destroy
   accepts_nested_attributes_for :profile
