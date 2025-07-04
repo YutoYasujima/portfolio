f6785842d10f1551a01715b15408fe7602b2babd
YutoYasujima <apyfm.3.25.y.y@gmail.com>
2025-07-04 22:43:51 +0900

    fixed:Chatのimage_width、image_heightをアップローダークラスで設定するように再修正

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,20 +2,7 @@ class Chat < ApplicationRecord
   belongs_to :user
   belongs_to :chatable, polymorphic: true
 
-  before_save :set_image_dimensions, if: -> { image.present? && image_changed? }
-
   mount_uploader :image, ChatImageUploader
 
   validates :message, length: { maximum: 500 }
-
-  private
-
-  # 画像の横幅と高さを設定する
-  def set_image_dimensions
-    return unless image&.file&.exists?
-
-    img = MiniMagick::Image.open(image.file.file)
-    self.image_width = img.width
-    self.image_height = img.height
-  end
 end
