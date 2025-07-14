17e6fb15463cdfb2fabd581cb206226e8bfce949
YutoYasujima <apyfm.3.25.y.y@gmail.com>
2025-07-14 15:16:46 +0900

    add:パスワードリセット機能追加

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -7,9 +7,32 @@ class Users::PasswordsController < Devise::PasswordsController
   # end
 
   # POST /resource/password
-  # def create
-  #   super
-  # end
+  def create
+    email = resource_params[:email].to_s.strip
+
+    if email.blank?
+      flash.now[:alert] = "メールアドレスを入力してください"
+      self.resource = resource_class.new # フォーム再描画に必要
+      render :new, status: :unprocessable_entity
+      return
+    end
+
+    self.resource = resource_class.find_by(email: email)
+
+    if resource.nil?
+      # メールアドレスが存在しない場合はDeviseのデフォルト動作
+      flash.now[:alert] = "入力されたメールアドレスは登録されていません"
+      self.resource = resource_class.new # フォーム再描画に必要
+      render :new, status: :unprocessable_entity
+    elsif resource.provider.present?
+      # Google認証ユーザーにはパスワードリセットメールを送らない
+      flash.now[:alert] = "Google認証で登録されたアカウントのため、パスワードのリセットはできません"
+      render :new, status: :unprocessable_entity
+    else
+      # 通常のメールアドレス・パスワードユーザーにはメール送信
+      super
+    end
+  end
 
   # GET /resource/password/edit?reset_password_token=abcdef
   # def edit
