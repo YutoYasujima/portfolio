9768308ff775f88051fb1b706d786b9c03ca7be5
YutoYasujima <apyfm.3.25.y.y@gmail.com>
2025-07-30 06:56:11 +0900

    add:ユーザーページにコミュニティ画面追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   include InfiniteScroll
 
+  before_action :set_user, only: %i[ communities load_more_communities ]
+
   USER_PER_PAGE = 12
   COMMUNITY_PER_PAGE = 12
 
@@ -23,7 +25,7 @@ class UsersController < ApplicationController
 
   # 参加中コミュニティ一覧
   def communities
-    total_records = current_user.approved_communities_with_member_count
+    total_records = @user.approved_communities_with_member_count
     load_init_data("communities", total_records, COMMUNITY_PER_PAGE)
     @communities = @records
     @communities_count = @total_records_count
@@ -33,12 +35,18 @@ class UsersController < ApplicationController
 
   # 参加中コミュニティ一覧の無限スクロール
   def load_more_communities
-    total_records = current_user.approved_communities_with_member_count
+    total_records = @user.approved_communities_with_member_count
     load_more_data("communities", total_records, COMMUNITY_PER_PAGE)
     # community_idをキー、各コミュニティにおけるユーザーの所属状態statusをバリューとしたハッシュを取得する
     @memberships_by_community_id = current_user.community_memberships.index_by(&:community_id)
     respond_to do |format|
       format.turbo_stream
     end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
