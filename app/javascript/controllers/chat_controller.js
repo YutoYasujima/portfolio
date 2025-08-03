eb6ba4405df41e51f0e56030b39888a6d6c26c77
YutoYasujima <apyfm.3.25.y.y@gmail.com>
2025-08-04 07:23:51 +0900

    add:コミュニティチャット画面で画面のトグル機能とリサイズ対応を追加

--- app/javascript/controllers/chat_controller.js
+++ app/javascript/controllers/chat_controller.js
@@ -1,11 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
 import consumer from "../channels/consumer"
+import debounce from "../lib/debounce"
+import { HEADER_HEIGHT, MOBILE_NAVIGATION_HEIGHT, BREAKPOINT_MOBILE} from "../lib/constants"
+
 
 // Connects to data-controller="chat"
 export default class extends Controller {
   static targets = [
     "container",
     "chatArea",
+    "nonChatArea",
+    "toggleIcon",
     "textareaForm",
     "textarea",
     "fileFieldForm",
@@ -27,9 +32,28 @@ export default class extends Controller {
   connect() {
     // チャット送信中フラグ
     this.isSending = false;
+    // レスポンシブの状態フラグ
+    this.isMobile = window.innerWidth < BREAKPOINT_MOBILE ? true : false;
+
+    // タイトル部分の要素の初期高さを保持しておく
+    this.openedNonChatAreaHeight = this.nonChatAreaTarget.clientHeight;
+    // トグルバーの高さ
+    this.toggleBarAreaHeight = 34;
+    // 入力フォームの高さ
+    this.inputFormAreaHeight = 40;
+    // チャットエリア外の固定要素の合計高さ(ヘッダー、トグルバー、入力フォーム)
+    this.nonChatFixAreaHeight = HEADER_HEIGHT + this.toggleBarAreaHeight + this.inputFormAreaHeight;
+
+    // チャットエリアの高さ設定
+    this.setChatAreaHeight(() => {
+      // 一番下にスクロール
+      this.scrollToBottom();
+      // ちらつき防止
+      this.containerTarget.classList.remove("opacity-0");
+    });
+
     // Action CableによるWebSocket通信
-    // 接続情報設定
-    // チャネルクラス名とIDを設定する
+    // 接続情報設定：チャネルクラス名とIDを設定する
     this.subscriptionInfo = {
       channel: this.channelNameValue,
       object_id: this.objectIdValue,
@@ -39,6 +63,41 @@ export default class extends Controller {
 
     // チャネルの購読開始
     this.subscribeChannel();
+
+    this.resizeChatAreaHeight = debounce(() => {
+      if (this.isMobile !== window.innerWidth < BREAKPOINT_MOBILE) {
+        // レスポンシブの変更あり
+        // タイトル部分を表示させておく
+        this.nonChatAreaTarget.style.height = null;
+        this.nonChatAreaTarget.classList.remove("non-chat-area-close");
+        this.toggleIconTarget.classList.remove("rotate-180");
+        // リサイズ時にタイトル部分の高さ保持変数の値を変更しておく
+        this.openedNonChatAreaHeight = this.nonChatAreaTarget.clientHeight;
+        this.isMobile = window.innerWidth < BREAKPOINT_MOBILE ? true : false;
+      }
+
+      // スクロール位置保持
+      const scrollPosition = this.containerTarget.scrollHeight - this.containerTarget.scrollTop;
+      // チャットエリア高さ設定
+      this.setChatAreaHeight();
+      // スクロール位置反映
+      this.containerTarget.scrollTop = this.containerTarget.scrollHeight - scrollPosition;
+    }, 200);
+    window.addEventListener("resize", this.resizeChatAreaHeight);
+  }
+
+  disconnect() {
+    window.removeEventListener("resize", this.resizeChatAreaHeight);
+  }
+
+  // チャットエリアの高さを設定する
+  setChatAreaHeight(func) {
+    this.containerTarget.style.height = null;
+    // チャットエリアの高さ算出(ビュー高さ-固定要素高さ-タイトル部分-ナビゲーション-余白)
+    let chatAreaHeight = window.innerHeight - (this.nonChatFixAreaHeight + this.nonChatAreaTarget.clientHeight + MOBILE_NAVIGATION_HEIGHT + 5);
+    this.containerTarget.style.height = `${chatAreaHeight}px`;
+    //高さが反映された直後の処理
+    func?.();
   }
 
   // チャネル購読
@@ -50,7 +109,6 @@ export default class extends Controller {
         // メソッド定義
         // ブロードキャスト受信後の処理
         received: async (data) => {
-          console.log('received');
           const chatId = data.chat_id;
           if (data.type === "create") {
             // チャット表示
@@ -119,6 +177,11 @@ export default class extends Controller {
     .catch(error => {
       console.error("部分テンプレートの取得に失敗:", error);
     });
+  }
+
+  // 一番下にスクロール
+  scrollToBottom() {
+    this.containerTarget.scrollTop = this.containerTarget.scrollHeight;
   }
 
   // スクロールがbottom付近か判定
@@ -214,6 +277,38 @@ export default class extends Controller {
     await this.sendChatData({
       formData,
       resetTarget: fileInput,
+    });
+  }
+
+  // チャットエリア外の要素の表示/非表示切り替え
+  toggleNonChatArea(event) {
+    if (this.nonChatAreaTarget.classList.contains("non-chat-area-close")) {
+      // チャットエリア外の要素が非表示中
+      this.adjustChatAreaHeight(0, this.openedNonChatAreaHeight);
+    } else {
+      // チャットエリア外の要素が表示中
+      this.adjustChatAreaHeight(this.openedNonChatAreaHeight, 0);
+    }
+    this.nonChatAreaTarget.classList.toggle("non-chat-area-close");
+    this.toggleIconTarget.classList.toggle("rotate-180");
+  }
+
+  // チャットエリアの高さ調整
+  adjustChatAreaHeight(beforeNonChatAreaHeight, afterNonChatAreaHeight) {
+    const scrollPosition = this.containerTarget.scrollHeight - this.containerTarget.scrollTop;
+    this.containerTarget.classList.add("opacity-0");
+
+    // 一度高さを明示
+    this.nonChatAreaTarget.style.height = `${beforeNonChatAreaHeight}px`;
+    requestAnimationFrame(() => {
+      this.nonChatAreaTarget.style.height = `${afterNonChatAreaHeight}px`;
+      this.nonChatAreaTarget.addEventListener("transitionend", () => {
+        // チャットエリアの高さを調整する
+        this.setChatAreaHeight(() => {
+          this.containerTarget.scrollTop = this.containerTarget.scrollHeight - scrollPosition;
+          this.containerTarget.classList.remove("opacity-0");
+        });
+      }, {once: true});
     });
   }
 
